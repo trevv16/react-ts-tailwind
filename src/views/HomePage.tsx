9210eedaf26e6c6d8c67fd797267d8653da5a5f6
trevv16 <tnjeru16@gmail.com>
2021-05-08 07:56:14 -0400

    adde seo tags to homepage

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { SeoHelmet } from '../components/index';
 
 export default function HomePage() {
   return (
     <>
+      <SeoHelmet title='' description='' image='' image_alt='' />
       <div></div>
     </>
   );
