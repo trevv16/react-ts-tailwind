9e27eb4a34a123c4dc4fcd68b6d17fb88e762a5d
trevv16 <tnjeru16@gmail.com>
2021-05-23 14:36:00 -0400

    refactored icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,11 +3,9 @@ import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 import { IoIosGlobe } from 'react-icons/io';
 
 export default function HomePage(props: any) {
-  const [name, setName] = useState<string>(props.name);
   const [size, setSize] = useState('w-6 h-6');
 
   useEffect(() => {
-    setName(props.iconName);
     if (props.size) {
       setSize(props.size);
     }
@@ -17,19 +15,15 @@ export default function HomePage(props: any) {
     switch (icon) {
       case 'instagram':
         return <FaInstagram className={`${size} flex-shrink-0 hover:text-gray-500`} />;
-        break;
 
       case 'github':
         return <FaGithub className={`${size} flex-shrink-0 hover:text-gray-500`} />;
-        break;
 
       case 'linkedin':
         return <FaLinkedin className={`${size} flex-shrink-0 hover:text-gray-500`} />;
-        break;
 
       case 'web':
         return <IoIosGlobe className={`${size} flex-shrink-0 hover:text-gray-500`} />;
-        break;
 
       default:
         return;
