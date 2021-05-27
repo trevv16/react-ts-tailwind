e27bf813e9e4a6b6aed5c87158bcd462a1e3aff4
trevv16 <tnjeru16@gmail.com>
2021-05-27 18:57:08 -0400

    added basic seo config info

--- src/Config.ts
+++ src/Config.ts
@@ -1,2 +1,5 @@
 export const SERVER_BASE_URL = 'http://localhost:5000';
 export const SITE_NAME = "Trevor's Portfolio";
+export const SITE_IMG = '';
+export const SITE_IMG_ALT = '';
+export const LAMBDA_URL = 'https://u3f6zkvkt2.execute-api.us-east-1.amazonaws.com/prod';
