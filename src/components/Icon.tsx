7b3caed094f43e02e025d6af325ba759433a643d
trevv16 <tnjeru16@gmail.com>
2021-06-08 18:02:10 -0400

    updated icon comp

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 import { IoIosGlobe } from 'react-icons/io';
 
-export default function HomePage(props: any) {
+type IconProps = {
+  name: string;
+  size?: string;
+};
+
+export default function Icon(props: IconProps) {
   const [size, setSize] = useState('w-6 h-6');
 
   useEffect(() => {
     if (props.size) {
       setSize(props.size);
     }
-  }, [props.iconName, props.size]);
+  }, [props.name, props.size]);
 
   const getSkillIcon = (icon: string) => {
     switch (icon) {
