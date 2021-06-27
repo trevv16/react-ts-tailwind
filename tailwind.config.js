c0834e718c061bbf40b5a3a6f8d1aedaef9ca49e
trevv16 <tnjeru16@gmail.com>
2021-06-26 22:39:28 -0400

    added tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: '#6C2978',
+      primaryLight: '#7C3D87',
+      secondary: '#CE91D9',
+      white: '#FFFFFF',
+      success: '#63A375',
+      successDark: '#EDAB31',
+      warning: '#F2C269',
+      warningDark: '#F0BA56',
+      danger: '#D01E11',
+      dangerDark: '#92140C',
+      gray: colors.coolGray
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
     extend: {}
   },
   variants: {
