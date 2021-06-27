9007774452b092adf8e4cc50c6374204913c4a8b
trevv16 <tnjeru16@gmail.com>
2021-06-26 22:28:21 -0400

    updated config variables

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,5 @@
 export const SITE_NAME = "Trevor's Portfolio";
-export const SITE_IMG = '';
-export const SITE_IMG_ALT = '';
+export const SITE_URL = 'https://domain.netlify.app';
+export const SITE_DESC = 'site description';
+export const SITE_IMG = 'https://source.unsplash.com/random';
 export const LAMBDA_URL = 'https://u3f6zkvkt2.execute-api.us-east-1.amazonaws.com/prod';
