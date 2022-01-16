04b6a9be8e736033a0048c3836eac9acdb8154ee
trevv21 <trevor@litterati.org>
2022-01-16 10:49:10 -0500

    updated seo impl

--- src/utils/seo.tsx
+++ src/utils/seo.tsx
@@ -1,15 +1,24 @@
-function getSeo(title: string, description: string, image: string, site_url: string) {
+import { SITE_NAME, SITE_URL, SITE_IMG, SITE_DESCRIPTION } from '../Config';
+
+function getSeo(
+  title: string | null = null,
+  description: string = SITE_DESCRIPTION,
+  image: string = SITE_IMG,
+  site_url: string = SITE_URL
+) {
+  const formattedTitle = title !== null ? `${title} | ${SITE_NAME}` : SITE_NAME;
+
   return (
     <>
-      <html lang='en' />
-      <title>{title}</title>
+      {/* <html lang="en" /> */}
+      <title>{formattedTitle}</title>
       <link rel='canonical' href={site_url} />
       <meta name='description' content={description} />
       {/* OG SEO */}
       <meta property='og:locale' content='en_us' />
-      <meta property='og:url' content={site_url} />s
+      <meta property='og:url' content={site_url} />
       <meta property='og:type' content='website' />
-      <meta property='og:title' content={title} />
+      <meta property='og:title' content={formattedTitle} />
       <meta property='og:description' content={description} />
       <meta property='og:image' content={image} />
       <meta property='og:image:alt' content={description} />
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,14 +1,11 @@
 import React, { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { SITE_NAME, SITE_URL, SITE_IMG, SITE_DESC } from '../Config';
 import { getSeo } from '../utils/seo';
 
 const HomePage: FC<any> = () => {
-  const title = `The Home of Business Ideas | ${SITE_NAME}`;
-
   return (
     <>
-      <Helmet>{getSeo(title, SITE_DESC, SITE_IMG, SITE_URL)}</Helmet>
+      <Helmet>{getSeo('The Home of Business Ideas')}</Helmet>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='max-w-3xl mx-auto my-80'></div>
       </div>
